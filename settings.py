bce05f73fde998d20ff927e938d7bc6068a036db
Deepanshu Lulla <deepanshu.lulla@gmail.com>
2017-09-01 20:29:43 +0000

    pg

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+import urlparse
 from base64 import b64encode
 
 random_bytes = os.urandom(64)
@@ -6,16 +7,18 @@ JWT_SECRET_KEY = b64encode(random_bytes).decode('utf-8')
 SECRET_KEY = JWT_SECRET_KEY
 DEBUG = True;
 
-DB_USERNAME = 'deepanshululla'
-# DB_PASSWORD = SECRET_KEY 
-DB_PASSWORD=''#not required for c9
-DATABASE_NAME = 'flask_rest_db'
-# DB must exists before application runs
-DB_HOST = os.getenv('DATABASE_URL', '0.0.0.0');
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-DB_URI = "postgresql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,DATABASE_NAME)
-# DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
-# uncomment for mYSQL
+DB_USERNAME = url.username
+# DB_PASSWORD = SECRET_KEY 
+DB_PASSWORD=url.password
+DATABASE_NAME = url.path[1:]
+# DB must exists before application runs
+DB_HOST = os.getenv(url.hostname, '0.0.0.0');
+DB_PORT =url.port
+DB_URI = "postgresql://%s:%s@%s:%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,DB_PORT, DATABASE_NAME)
+
 
 SQLALCHEMY_DATABASE_URI = DB_URI;
 SQLALCHEMY_TRACK_MODIFICATIONS = True;
