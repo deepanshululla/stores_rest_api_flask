811620993db47bd6e2c288a38e52ab5dffda6b87
Deepanshu Lulla <deepanshu.lulla@gmail.com>
2017-09-01 21:13:51 +0000

    c9

--- settings.py
+++ settings.py
@@ -7,23 +7,15 @@ JWT_SECRET_KEY = b64encode(random_bytes).decode('utf-8')
 SECRET_KEY = JWT_SECRET_KEY
 DEBUG = True;
 
+
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 DB_URL= os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 
-DB_USERNAME = 'deepanshululla'
-# DB_PASSWORD = SECRET_KEY 
-DB_PASSWORD=''#not required for c9
-DATABASE_NAME = 'flask_rest_db'
-# DB must exists before application runs
-DB_HOST = os.getenv('IP', '0.0.0.0');
+
 
 # DB_URI = "postgresql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST,DATABASE_NAME)
-DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
-# uncomment for mYSQL
 
 SQLALCHEMY_DATABASE_URI = DB_URL;
-SQLALCHEMY_TRACK_MODIFICATIONS = True;
-# SET it true if enabling migrations
-# Basically SQLACHEMY has its own tracking mechanism so we are saying we don't need flask_SQL_ALCHEMy for it
+SQLALCHEMY_TRACK_MODIFICATIONS = False;
 
