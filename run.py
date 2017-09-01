3d1e49c53e94aadd4d9d5b310408dfd7f33988bb
Deepanshu Lulla <deepanshu.lulla@gmail.com>
2017-09-01 18:28:48 +0000

    Fixing DB

---
+++ run.py
@@ -0,0 +1,12 @@
+from app import app
+from db import db
+
+app.config.from_object('settings')
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    """
+        creates all tables before the first request is sent. But the database must exist beforehand
+    """
+    db.create_all()
